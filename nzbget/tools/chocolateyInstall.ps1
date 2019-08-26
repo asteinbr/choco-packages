78aa377aab88859fed6557023615423745036e69
asteinbr <asteinbr@gmail.com>
2019-08-26 23:04:42 +0200

    checksum added

--- nzbget/tools/chocolateyInstall.ps1
+++ nzbget/tools/chocolateyInstall.ps1
@@ -12,9 +12,9 @@ $packageArgs = @{
 
   softwareName  = 'nzbget*'
 
-  checksum      = ''
+  checksum      = '11119d24a8889a0377b3bc5697f81b4ae879e70c6e67fd0392809d2b6038746d'
   checksumType  = 'sha256'
-  checksum64    = ''
+  checksum64    = '11119d24a8889a0377b3bc5697f81b4ae879e70c6e67fd0392809d2b6038746d'
   checksumType64= 'sha256'
 
   silentArgs   = '/S'
