6d4bfcab1e73515d400379ee076a580a5c74e0e0
asteinbr <asteinbr@gmail.com>
2019-08-27 21:38:54 +0200

    removed 64-bit entries

--- nzbget/tools/chocolateyInstall.ps1
+++ nzbget/tools/chocolateyInstall.ps1
@@ -1,21 +1,17 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url        = 'https://github.com/nzbget/nzbget/releases/download/v21.0/nzbget-21.0-bin-windows-setup.exe'
-$url64      = 'https://github.com/nzbget/nzbget/releases/download/v21.0/nzbget-21.0-bin-windows-setup.exe'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'EXE'
   url           = $url
-  url64bit      = $url64
 
   softwareName  = 'nzbget*'
 
   checksum      = '11119d24a8889a0377b3bc5697f81b4ae879e70c6e67fd0392809d2b6038746d'
   checksumType  = 'sha256'
-  checksum64    = '11119d24a8889a0377b3bc5697f81b4ae879e70c6e67fd0392809d2b6038746d'
-  checksumType64= 'sha256'
 
   silentArgs   = '/S'
   validExitCodes= @(0)
